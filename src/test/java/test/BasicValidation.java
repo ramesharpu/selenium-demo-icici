c4f706ea1bd4252ccc291d73c3dbc08afeab90c4
Ramesh A <ramesharpu@gmail.com>
2025-02-10 15:33:52 +0530

    updated the test code

--- src/test/java/test/BasicValidation.java
+++ src/test/java/test/BasicValidation.java
@@ -1,5 +1,8 @@
 package test;
 
+import static org.testng.Assert.assertTrue;
+
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class BasicValidation extends BaseTest{
@@ -8,6 +11,11 @@ public class BasicValidation extends BaseTest{
 	public void urlValidation() {
 		String expectedResult = "https://www.facebook.com/";
 		String actualResult = driver.getCurrentUrl();
+		
+		Reporter.log("Expected Result - " + expectedResult);
+		Reporter.log("Actual Result - " + actualResult );
+		
+		assertTrue(actualResult.equals(expectedResult), "Mismatch in the URL");
 	}
 
 }
